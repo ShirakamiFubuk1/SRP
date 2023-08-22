c611ebed796f732b27b885b3835de40adf7e1617
mona <1179515821@qq.com>
2023-08-22 10:57:48 +0800

    Upload PerObjectMaterialProperties component

---
+++ Assets/Custom RP/Examples/PerObjectMaterialProperties.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PerObjectMaterialProperties : MonoBehaviour
+{
+    private static int baseColorId = Shader.PropertyToID("_BaseColor");
+
+    [SerializeField] private Color baseColor = Color.white;
+}
