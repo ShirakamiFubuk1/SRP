9098a278dee7e659e56a294d27020eed9d8c76dd
mona <1179515821@qq.com>
2023-08-23 14:07:01 +0800

    add Preset for Unlit Shader

--- Assets/Custom RP/Editor/CustomShaderGUI.cs
+++ Assets/Custom RP/Editor/CustomShaderGUI.cs
@@ -29,9 +29,24 @@ public class CustomShaderGUI : ShaderGUI
         }
     }
 
-    void SetProperty(string name, float value)
+    bool SetProperty(string name, float value)
     {
-        FindProperty(name, properties).floatValue = value;
+        MaterialProperty property = FindProperty(name, properties, false);
+        if (property != null)
+        {
+            property.floatValue = value;
+            return true;
+        }
+
+        return false;
+    }
+
+    void SetProperty(string name, string keyword, bool value)
+    {
+        if (SetProperty(name, value ? 1f : 0f))
+        {
+            SetKeyword(keyword,value);
+        }
     }
 
     void SetKeyword(string keyword, bool enable)
@@ -51,13 +66,7 @@ public class CustomShaderGUI : ShaderGUI
             }
         }
     }
-
-    void SetProperty(string name, string keyword, bool value)
-    {
-        SetProperty(name,value?1f:0f);
-        SetKeyword(keyword,value);
-    }
-
+    
     private bool Clipping
     {
         set => SetProperty("_Clipping", "_CLIPPING", value);
