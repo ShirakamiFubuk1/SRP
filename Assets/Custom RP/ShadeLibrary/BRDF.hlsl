fb573eb729867f1ed822933071e413da45625869
mona <1179515821@qq.com>
2023-08-23 10:48:41 +0800

    bug fix

--- Assets/Custom RP/ShadeLibrary/BRDF.hlsl
+++ Assets/Custom RP/ShadeLibrary/BRDF.hlsl
@@ -4,7 +4,6 @@
 #define MIN_REFLECTIVITY 0.04
 
 #include "Surface.hlsl"
-#include "Common.hlsl"
 
 struct BRDF
 {
@@ -16,7 +15,7 @@ struct BRDF
 float OneMinusReflectivity(float metallic)
 {
     float range = 1.0 - MIN_REFLECTIVITY;
-    return range(1-metallic);
+    return range * (1-metallic);
 }
 
 BRDF GetBRDF(Surface surface)
@@ -24,29 +23,28 @@ BRDF GetBRDF(Surface surface)
     BRDF brdf;
 
     float oneMinusReflectivity = OneMinusReflectivity(surface.metallic);
-    float perceptualRoughness = PerceptualSmoothnessToPerceptualRoughness(surface.smoothness);
-    
+
     brdf.diffuse = surface.color * oneMinusReflectivity;
-    brdf.specular = lerp(MIN_REFLECTIVITY,surface.color,surface.metallic);
-    brdf.roughness = PerceptualRoughnessToRoughness(perceptualRoughness);
+    brdf.specular = 0.0;
+    brdf.roughness = 0.0;
 
     return brdf;
 }
 
-float SpecularStrength(Surface surface,BRDF brdf,Light light)
+float SpecularStrength (Surface surface, BRDF brdf, Light light)
 {
     float3 h = SafeNormalize(light.direction + surface.viewDirection);
-    float nh2 = Square(saturate(dot(surface.normal,h)));
-    float lh2 = Square(saturate(dot(light.direction,h)));
+    float nh2 = Square(saturate(dot(surface.normal, h)));
+    float lh2 = Square(saturate(dot(light.direction, h)));
     float r2 = Square(brdf.roughness);
-    float d2 = Square(nh2 * (r2-1.0)+1.00001);
+    float d2 = Square(nh2 * (r2 - 1.0) + 1.00001);
     float normalization = brdf.roughness * 4.0 + 2.0;
-    return r2/(d2*max(0.1,lh2)*normalization);
+    return r2 / (d2 * max(0.1, lh2) * normalization);
 }
 
 float3 DirectBRDF(Surface surface,BRDF brdf,Light light)
 {
-    return SpecularStrength(surface,brdf,light) * brdf.specular + brdf.diffuse;
+    return SpecularStrength(surface,brdf,light)*brdf.specular+brdf.diffuse;
 }
 
 #endif
