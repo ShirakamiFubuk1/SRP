e4179d3d32b36ee3b935e939644148f1752d7526
mona <1179515821@qq.com>
2023-08-23 15:37:52 +0800

    support shadow culling

--- Assets/Custom RP/Runtime/Shadow.cs
+++ Assets/Custom RP/Runtime/Shadow.cs
@@ -11,12 +11,38 @@ public class Shadows
         name = bufferName
     };
 
+    private const int maxShadowedDirectionalLightCount = 1;
+
     private ScriptableRenderContext context;
 
     private CullingResults cullingResults;
 
     private ShadowSettings setting;
 
+    private int ShadowedDirectionalLightCount;
+    
+     struct ShadowedDirectionalLight
+     {
+         public int visibleLightIndex;
+     }
+
+    private ShadowedDirectionalLight[] shadowedDirectionalLights =
+        new ShadowedDirectionalLight[maxShadowedDirectionalLightCount];
+
+    public void ReserveDirectionalShadows(Light light, int visibleLightIndex)
+    {
+        if (ShadowedDirectionalLightCount < maxShadowedDirectionalLightCount 
+            && light.shadows != LightShadows.None 
+            && light.shadowStrength > 0f 
+            && cullingResults.GetShadowCasterBounds(visibleLightIndex,out Bounds b))
+        {
+            shadowedDirectionalLights[ShadowedDirectionalLightCount++] = new ShadowedDirectionalLight
+            {
+                visibleLightIndex = visibleLightIndex
+            };
+        }
+    }
+    
     public void Setup(
         ScriptableRenderContext context,CullingResults cullingResults,ShadowSettings setting
     )
@@ -24,6 +50,8 @@ public class Shadows
         this.context = context;
         this.cullingResults = cullingResults;
         this.setting = setting;
+
+        ShadowedDirectionalLightCount = 0;
     }
 
     void ExecuteBuffer()
