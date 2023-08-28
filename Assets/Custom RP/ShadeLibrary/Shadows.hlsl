b2752b902b515b82c25cb828e18c0e0deed8d729
mona <1179515821@qq.com>
2023-08-28 10:21:02 +0800

    shadow cascade blend

--- Assets/Custom RP/ShadeLibrary/Shadows.hlsl
+++ Assets/Custom RP/ShadeLibrary/Shadows.hlsl
@@ -34,6 +34,7 @@ CBUFFER_END
 struct ShadowData
 {
     int cascadeIndex;
+    float cascadeBlend;
     float strength;
 };
 
@@ -52,6 +53,7 @@ float FadedShadowStrength(float distance, float scale,float fade)
 ShadowData GetShadowData(Surface surfaceWS)
 {
     ShadowData data;
+    data.cascadeBlend = 1.0;
     data.strength = FadedShadowStrength(surfaceWS.depth,_ShadowDistanceFade.x,_ShadowDistanceFade.y);
     int i;
     for(i = 0;i<_CascadeCount;i++)
@@ -60,9 +62,14 @@ ShadowData GetShadowData(Surface surfaceWS)
         float distanceSqr = DistanceSquared(surfaceWS.position,sphere.xyz);
         if(distanceSqr < sphere.w)
         {
+            float fade = FadedShadowStrength(distanceSqr,_CascadeData[i].x,_ShadowDistanceFade.z);
             if(i==_CascadeCount - 1)
             {
-                data.strength *= FadedShadowStrength(distanceSqr,_CascadeData[i].x,_ShadowDistanceFade.z);
+                data.strength *= fade;
+            }
+            else
+            {
+                data.cascadeBlend = fade;
             }
             break;
         }
@@ -113,6 +120,13 @@ float GetDirectionalShadowAttenuation (DirectionalShadowData directional,ShadowD
     float3 positionSTS = mul(_DirectionalShadowMatrices[directional.tileIndex],float4(surfaceWS.position + normalBias,1.0)).xyz;
 
     float shadow = FilterDirectionalShadow(positionSTS);
+
+    if(global.cascadeBlend<1.0)
+    {
+        normalBias = surfaceWS.normal * (directional.normalBias * _CascadeData[global.cascadeIndex +1].y);
+        positionSTS = mul(_DirectionalShadowMatrices[directional.tileIndex+1],float4(surfaceWS.position+normalBias,1.0)).xyz;
+        shadow = lerp(FilterDirectionalShadow(positionSTS),shadow,global.cascadeBlend);
+    }
 
     return lerp(1.0,shadow,directional.strength);
 }
